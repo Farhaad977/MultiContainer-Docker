17f4e3b6d889b119bc09c9cba7e6ed60efd5c1e7
Farhaad977 <farhaad.sheikhmohammed002@stud.fh-dortmund.de>
2025-07-17 16:00:41 +0200

    Modified index.js Listen Port

--- server/index.js
+++ server/index.js
@@ -89,7 +89,8 @@ const redisPublisher = redisClient.duplicate();
     }
   });
 
-  app.listen(5000, () => {
-    console.log('Listening on port 5000');
-  });
+app.listen(5000, '0.0.0.0', () => {
+  console.log('Listening on port 5000');
+});
+
 })();
